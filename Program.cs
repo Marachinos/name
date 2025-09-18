54f57a47d9f8b9311cff527eb950db1c48417486
Marachinos <stallnysater@gmail.com>
2025-09-18 20:02:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace namn
+{
+    internal class Program
+    {
+        static string FormatName(string first, string third)
+        {
+            return $"{first} {third}";
+        }
+        static string FormatName(string first, string second, string third)
+        {
+            return $"{first} {second} {third}";
+        }
+        static string FormatName(string first, string third, bool uppercase)
+        {
+            var full = $"{first} {third}";
+            return uppercase ? full.ToUpper() : full;
+        }
+        static void Main(string[] args)
+        {
+        Console.WriteLine(FormatName("Sandra", "Jonsson"));
+            Console.WriteLine(FormatName("Sandra", "Elisabet Maria", "Jonsson"));
+        Console.WriteLine(FormatName("Sandra", "Jonsson", true)); 
+        }
+    }
+}
