039ba4664606ef3a49f7d224202f258d63790de4
Marachinos <stallnysater@gmail.com>
2025-09-18 20:20:50 +0200

    Added 3 new names and a commit

--- Program.cs
+++ Program.cs
@@ -20,7 +20,11 @@
             //skriver ut namnen i 3 olika format
          Console.WriteLine(FormatName("Sandra", "Jonsson"));
             Console.WriteLine(FormatName("Sandra", "Elisabet Maria", "Jonsson"));
-        Console.WriteLine(FormatName("Sandra", "Jonsson", true)); 
+        Console.WriteLine(FormatName("Sandra", "Jonsson\n", true));
+            //skriver ut 3 nya namn i 3 olika format
+            Console.WriteLine(FormatName("Martin", "Jonsson"));
+            Console.WriteLine(FormatName("Martin", "Helmer", "Jonsson"));
+            Console.WriteLine(FormatName("Martin", "Jonsson", true));
         }
     }
 }
